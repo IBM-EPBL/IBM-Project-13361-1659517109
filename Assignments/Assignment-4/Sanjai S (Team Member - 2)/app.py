5cc9adb2ef8ce7b08e7c55e8790482505eef40e9
UdhayakumaranH <udhaya2692@gmail.com>
2022-11-26 23:09:43 +0530

    upload

---
+++ Assignments/Assignment-4/Sanjai S (Team Member - 2)/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'assignment-4'
+
+@app.route("/")
+def home():
+    return render_template("page-login.html")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
